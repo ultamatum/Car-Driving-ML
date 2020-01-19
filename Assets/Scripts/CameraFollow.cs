f209b23cccbc707d4afa6c152a0117570f468324
Ultamatum <alexanderg0502@gmail.com>
2020-01-19 10:17:24 +0000

    Nicer Car stuff
    -Stole car from asset store
    -Made car drive nicer

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,18 +3,29 @@
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float smoothSpeed = 0.125f;
     public Vector3 offset;
+    public float followSpeed = 10;
+    public float lookSpeed = 10;
 
     private Vector3 velocity = Vector3.zero;
 
-    private void LateUpdate()
+    public void LookAtTarget()
     {
-        Vector3 desiredPosition = target.TransformPoint(offset);
-        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+        Vector3 lookDir = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(lookDir, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, lookSpeed * Time.deltaTime);
+    }
 
-        transform.position = smoothedPosition;
+    public void MoveToTarget()
+    {
+        Vector3 desiredPos = target.position + target.forward * offset.z + target.right * offset.x + target.up * offset.y;
 
-        transform.LookAt(target);
+        transform.position = Vector3.Lerp(transform.position, desiredPos, followSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        LookAtTarget();
+        MoveToTarget();
     }
 }
--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -29,18 +29,37 @@ public class CarController : MonoBehaviour
 
     void Accelerate()
     {
-        backRightW.motorTorque = torque * verticalInput;
-        backLeftW.motorTorque = torque * verticalInput;
+        if(verticalInput >= 0.0f)
+        {
+            backRightW.motorTorque = torque * verticalInput;
+            backLeftW.motorTorque = torque * verticalInput;
+        } else if (verticalInput <= 0.0f)
+        {
+            backRightW.brakeTorque = torque * verticalInput * -1;
+            backLeftW.brakeTorque = torque * verticalInput * -1;
+
+
+            Debug.Log(verticalInput);
+        }
     }
 
     void UpdateWheelPoses()
     {
-
+        UpdateWheelPose(frontRightW, frontRightT);
+        UpdateWheelPose(frontLeftW, frontLeftT);
+        UpdateWheelPose(backRightW, backRightT);
+        UpdateWheelPose(backLeftW, backLeftT);
     }
 
-    void UpdateWheelPose(WheelCollider collider, Transform transform)
+    void UpdateWheelPose(WheelCollider coll, Transform trans)
     {
+        Vector3 pos = trans.position;
+        Quaternion rot = trans.rotation;
 
+        coll.GetWorldPose(out pos, out rot);
+
+        trans.position = pos;
+        trans.rotation = rot;
     }
 
     private void FixedUpdate()
