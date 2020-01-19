0aa13b835451b6ab11e6d71ce080415f92d98451
Ultamatum <alexanderg0502@gmail.com>
2020-01-19 07:17:32 +0000

    Made a Car :D
    Car drives now, need to mess around with mass etc

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.TransformPoint(offset);
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public WheelCollider frontRightW, frontLeftW, backRightW, backLeftW;
+    public Transform frontRightT, frontLeftT, backRightT, backLeftT;
+    public float maxSteerAngle = 30;
+    public float torque = 100;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float steeringAngle;
+
+    void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    void Steer()
+    {
+        steeringAngle = maxSteerAngle * horizontalInput;
+
+        frontRightW.steerAngle = steeringAngle;
+        frontLeftW.steerAngle = steeringAngle;
+    }
+
+    void Accelerate()
+    {
+        backRightW.motorTorque = torque * verticalInput;
+        backLeftW.motorTorque = torque * verticalInput;
+    }
+
+    void UpdateWheelPoses()
+    {
+
+    }
+
+    void UpdateWheelPose(WheelCollider collider, Transform transform)
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelerate();
+        UpdateWheelPoses();
+    }
+}
